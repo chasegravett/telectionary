68efea28375c8f630d352f63239154a8cf7fee06
Chase Gravett <garrisongravett@gmail.com>
2023-12-31 20:20:47 -0600

    General Flask file structure added. Navbar placeholder for use as layout template

--- app.py
+++ app.py
@@ -1,1 +1,26 @@
-print("This is for pushing everything to github")
+from flask import Flask, render_template, request, flash, redirect, session
+from flask_session import Session
+from werkzeug.security import check_password_hash, generate_password_hash
+
+
+app = Flask(__name__)
+
+
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+
+@app.route("/", methods=["GET"])
+def index():
+    
+    return render_template("home.html")
